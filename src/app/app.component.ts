4858496bdcafd51efb8a3c291fcca2f3b5d677b5
Italo Siqueira <siqueira.italo@gmail.com>
2019-01-30 10:39:21 -0300

    add files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TesteImgGoogleMaps';
+  private apiKey = 'YOU_API_KEY';
+  private makerColor = 'red';
+  private coords = '-23.572278,-46.664689';
+  private imgSize = '800x600';
+  private zoom = '18';
+
+  getUrlGoogleMaps(): string {
+    // tslint:disable-next-line:max-line-length
+    const url = `http://maps.googleapis.com/maps/api/staticmap?zoom=${this.zoom}&size=${this.imgSize}&markers=size:mid|color:${this.makerColor}|${this.coords}&sensor=false&key=${this.apiKey}`;
+
+    return url;
+  }
+
 }
